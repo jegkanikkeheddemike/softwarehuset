400f8d56c7a0c983916c4fcca2aa4bd0ce97b3ed
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-04-16 12:47:14 +0200

    kommentar

--- src/main/java/dtu/mennekser/softwarehuset/app/windows/home/ActivityMenu.java
+++ src/main/java/dtu/mennekser/softwarehuset/app/windows/home/ActivityMenu.java
@@ -14,6 +14,12 @@ public class ActivityMenu extends BorderPane implements HasDBConnection {
     BorderPane assignedPane;
     DBSubscriber<Activity> activitySubscriber;
 
+
+
+    //Det her gør at hvis der kommer ændringer på projektet mens den er åben bliver det ikke reflekteret.
+    //Men det er vel ok siden man ikke kan ændre på projektnavnet osv.
+    //Måske kan det fikses men det er ok som det er indtil videre.
+
     public ActivityMenu(Project project,int activityID){
         activitySubscriber = new DBSubscriber<>(database -> database.projects.get(project.id).activities.get(activityID), activity -> {
 
