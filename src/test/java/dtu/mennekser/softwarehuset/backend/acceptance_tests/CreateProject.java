a59cea41f509b056388cfffa76e6d325fa3b9b13
Pinkymus <katinkaspangtoft@gmail.com>
2023-04-17 17:50:53 +0200

    It a works

---
+++ src/test/java/dtu/mennekser/softwarehuset/backend/acceptance_tests/CreateProject.java
@@ -0,0 +1,51 @@
+package dtu.mennekser.softwarehuset.backend.acceptance_tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CreateProject {
+
+
+    @Given("user is logged in")
+    public void user_is_logged_in() {
+
+    }
+    @When("user creates a project")
+    public void user_creates_a_project() {
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("a project is created")
+    public void a_project_is_created() {
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the project is added to the list of projects")
+    public void the_project_is_added_to_the_list_of_projects() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("user is not logged in")
+    public void user_is_not_logged_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("error message {string} is given")
+    public void error_message_is_given(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("a client {string} is added to the project")
+    public void a_client_is_added_to_the_project(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("that project has a client {string}")
+    public void that_project_has_a_client(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
