7b353e8795af57a9b25ff1cb9288233128e30eaf
Pinkymus <katinkaspangtoft@gmail.com>
2023-04-17 17:39:19 +0200

    aaaarh det virker ikke'

---
+++ src/test/java/dtu/library/acceptance_tests/CreateProject/CreateProject.java
@@ -0,0 +1,13 @@
+package dtu.library.acceptance_tests.CreateProject;
+
+import io.cucumber.java.en.Given;
+
+public class CreateProject {
+
+
+    @Given("user is logged in")
+    public void user_is_logged_in() {
+        System.out.println("Hejsa");
+    }
+
+}
