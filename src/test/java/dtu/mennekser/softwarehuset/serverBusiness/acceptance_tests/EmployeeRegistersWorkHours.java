08c2f7756dea93a46be9754a7ef7e7f7c01eae93
Pinkymus <katinkaspangtoft@gmail.com>
2023-04-23 12:19:59 +0200

    acceptance test

--- src/test/java/dtu/mennekser/softwarehuset/serverBusiness/acceptance_tests/EmployeeRegistersWorkHours.java
+++ src/test/java/dtu/mennekser/softwarehuset/serverBusiness/acceptance_tests/EmployeeRegistersWorkHours.java
@@ -1,18 +1,35 @@
 package dtu.mennekser.softwarehuset.serverBusiness.acceptance_tests;
 
+import dtu.mennekser.softwarehuset.backend.schema.AppBackend;
+import dtu.mennekser.softwarehuset.backend.schema.Session;
+import dtu.mennekser.softwarehuset.backend.schema.TimeRegistration;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import static org.junit.Assert.assertTrue;
+
 public class EmployeeRegistersWorkHours {
 
-    @When("the user registers {int} work hours to {string}")
-    public void the_user_registers_work_hours_to(Integer int1, String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    AppBackend appBackend;
+    String error;
+    Session session;
+    int projectID;
+    int activityID;
+    int employeeID;
+
+    public EmployeeRegistersWorkHours() {
+        appBackend = new AppBackend();
+        employeeID = appBackend.createEmployee("Hanne");
     }
-    @Then("the {int} work hours are registered to {string}")
-    public void the_work_hours_are_registered_to(Integer int1, String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
+
+    @When("the user registers {string} work hours to {string}")
+    public void the_user_registers_work_hours_to(String hours, String string) {
+        appBackend.registerTime(projectID,activityID,hours,session);
+    }
+    @Then("the {string} work hours are registered to {string}")
+    public void the_work_hours_are_registered_to(String hours, String string) {
+        //assertTrue(appBackend.getTimeRegistrationsOfActivity(projectID,activityID,session).contains());
     }
 
 }
