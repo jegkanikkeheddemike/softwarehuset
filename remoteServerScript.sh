a74210fd4c8d6a941b13342c51bbb93a3eb44e83
jegkanikkeheddemike <54175974+jegkanikkeheddemike@users.noreply.github.com>
2023-04-23 12:42:46 +0200

    skip tets

--- remoteServerScript.sh
+++ remoteServerScript.sh
@@ -2,5 +2,5 @@ while true
 do
   git reset --hard
   git pull
-  /usr/bin/mvn clean install compile exec:java -Dexec.mainClass=dtu.mennekser.softwarehuset.ServerMain
+  /usr/bin/mvn clean install compile exec:java -Dexec.mainClass=dtu.mennekser.softwarehuset.ServerMain -Dmaven.test.skip=true
 done
