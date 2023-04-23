ca1cb20ffd342160047e4b556c3ed53a09695d7c
Pinkymus <katinkaspangtoft@gmail.com>
2023-04-23 11:54:31 +0200

    more stuff

---
+++ src/test/java/dtu/mennekser/softwarehuset/serverBusiness/acceptance_tests/EmployeeRegistersWorkHours.java
@@ -0,0 +1,18 @@
+package dtu.mennekser.softwarehuset.serverBusiness.acceptance_tests;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+public class EmployeeRegistersWorkHours {
+
+    @When("the user registers {int} work hours to {string}")
+    public void the_user_registers_work_hours_to(Integer int1, String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the {int} work hours are registered to {string}")
+    public void the_work_hours_are_registered_to(Integer int1, String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
