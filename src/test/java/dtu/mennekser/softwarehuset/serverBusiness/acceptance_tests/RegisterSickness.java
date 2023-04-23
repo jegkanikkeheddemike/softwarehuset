9060431101f13e2c2b423d7eabd14b49f4c46cde
Pinkymus <katinkaspangtoft@gmail.com>
2023-04-23 15:22:05 +0200

    Flere test classer

---
+++ src/test/java/dtu/mennekser/softwarehuset/serverBusiness/acceptance_tests/RegisterSickness.java
@@ -0,0 +1,27 @@
+package dtu.mennekser.softwarehuset.serverBusiness.acceptance_tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegisterSickness {
+    public RegisterSickness(){}
+
+    @When("the user registers that they are sick in week {int}")
+    public void theUserRegistersThatTheyAreSickInWeek(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the user is registered as sick in week {int}")
+    public void theUserIsRegisteredAsSickInWeek(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("{string} is sick in week {int}")
+    public void isSickInWeek(String string, Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
---
+++ src/test/java/dtu/mennekser/softwarehuset/serverBusiness/acceptance_tests/RegisterVacation.java
@@ -0,0 +1,34 @@
+package dtu.mennekser.softwarehuset.serverBusiness.acceptance_tests;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Given;
+
+public class RegisterVacation {
+
+    public RegisterVacation(){}
+
+    @When("the user register a vacation from week {string} to week {string}")
+    public void theUserRegisterAVacationFromWeekToWeek(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("a vacation from week {string} to week {string} is registered for that user")
+    public void aVacationFromWeekToWeekIsRegisteredForThatUser(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Given("the user has a vacation from week {string} to week {string}")
+    public void theUserHasAVacationFromWeekToWeek(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("an activity {string} with start week {string} and end week {string} exists")
+    public void anActivityWithStartWeekAndEndWeekExists(String string, String string2, String string3) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
