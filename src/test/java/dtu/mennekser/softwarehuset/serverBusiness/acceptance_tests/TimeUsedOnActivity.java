1dde458b2183d0ec92b4354814bb5026d770abeb
Toby <t.f.martinsen@gmail.com>
2023-04-23 13:13:01 +0200

    Create TimeUsedOnActivity.java

---
+++ src/test/java/dtu/mennekser/softwarehuset/serverBusiness/acceptance_tests/TimeUsedOnActivity.java
@@ -0,0 +1,26 @@
+package dtu.mennekser.softwarehuset.serverBusiness.acceptance_tests;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TimeUsedOnActivity {
+    public TimeUsedOnActivity() {
+    }
+
+    @Given("activity has registered time")
+    public void activity_has_registered_time() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("user checks time used on activity")
+    public void user_checks_time_used_on_activity() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user gets time used on activity")
+    public void user_gets_time_used_on_activity() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
