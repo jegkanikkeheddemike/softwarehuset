6b9b729191f96c43f616088ae1eb73d987e70f73
Fewrick <54175840+Fewrick@users.noreply.github.com>
2023-04-24 13:31:04 +0200

    localhost

--- src/main/java/dtu/mennekser/softwarehuset/app/windows/home/ProjectMenu.java
+++ src/main/java/dtu/mennekser/softwarehuset/app/windows/home/ProjectMenu.java
@@ -86,7 +86,7 @@ public class ProjectMenu extends BorderPane {
                     Style.setActivityButtonStyle(button);
                     button.setOnAction(actionEvent -> HomePage.setActivity(projectName,projectID,activity));
                     activitiesPane.getChildren().add(buttonStack);
-                    
+
                 }
             }
         );
