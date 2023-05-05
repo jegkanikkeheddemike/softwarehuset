102f4bfe8fd136e468a40a590cf05944540ccbff
Toby <t.f.martinsen@gmail.com>
2023-05-05 15:08:46 +0200

    WB findProject cucmber

---
+++ src/test/java/dtu/mennekser/softwarehuset/whiteBoxTests/WB_findProject.java
@@ -0,0 +1,39 @@
+package dtu.mennekser.softwarehuset.whiteBoxTests;
+
+import dtu.mennekser.softwarehuset.backend.schema.AppBackend;
+import dtu.mennekser.softwarehuset.backend.schema.Project;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.Assert.*;
+
+public class WB_findProject {
+    static AppBackend appBackend;
+    static Project project;
+    public WB_findProject() {appBackend = new AppBackend();}
+    @Given("projects is empty")
+    public void projectsIsEmpty() {
+    }
+
+    @When("findProject is called with name {string}")
+    public void findProjectIsCalledWithName(String string) {
+        project = appBackend.findProject(string);
+    }
+
+    @Then("findProject returns null")
+    public void findProjectReturnsNull() {
+        assertNull(project);
+    }
+
+    @Given("project {string} exist")
+    public void projectExist(String string) {
+        appBackend.createEmployee("hann");
+        appBackend.createProject(string, "No one", appBackend.attemptLogin("hann"), "1");
+    }
+
+    @Then("findProject returns project {string}")
+    public void findProjectReturnsProject(String string) {
+        assertTrue(project.name.equals(string));
+    }
+}
